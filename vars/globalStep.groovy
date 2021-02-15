5f4ee7758163b9bde98851391c7ca70b55020e21
johnreah <john.reah@johnreah.com>
2021-02-15 18:32:20 +0000

    Experimenting with directories

--- vars/globalStep.groovy
+++ vars/globalStep.groovy
@@ -1,9 +1,31 @@
 #!/usr/bin/env groovy
 import com.johnreah.jenkins.Artifact
+import groovy.json.JsonSlurper
 
 def call(List<Artifact> artifacts) {
     artifacts.each {
+        findInNexus it
         println "Artifact: " + it.toString()
     }
 }
 
+def findInNexus(Artifact artifact) {
+    def connection = new URL(""
+            + "http://localhost:8081/service/rest"
+            + '/v1/search'
+            + "?repository=maven-public"
+            + "&group=${artifact.groupId}"
+            + "&name=${artifact.artifactId}"
+            + "&sort=version"
+            + "&direction=desc")
+            .openConnection() as HttpURLConnection
+    connection.setRequestProperty('Accept', 'application/json')
+    if (connection.responseCode == 200) {
+        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
+        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
+        artifact.version = asset.maven2.version
+        artifact.url = asset.downloadUrl
+    } else {
+        throw new Exception("connection error")
+    }
+}
