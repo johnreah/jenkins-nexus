3c1f3ad9ee249efbfbfbcfac347ee653606ac02d
johnreah <john.reah@johnreah.com>
2021-02-15 15:02:37 +0000

    Experimenting with directories

---
+++ vars/globalStep.groovy
@@ -0,0 +1,6 @@
+#!/usr/bin/env groovy
+
+def call(String input = 'unspecified') {
+    println "This is globalStep, value = " + input
+}
+
--- vars/globvar.groovy
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env groovy
-
-def globalMethod() {
-    println "This is globvar"
-}
-
