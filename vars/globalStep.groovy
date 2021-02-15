848b25ef997653fb6265d5c6573f8d5a69470398
johnreah <john.reah@johnreah.com>
2021-02-15 18:35:49 +0000

    Experimenting with directories

--- vars/globalStep.groovy
+++ vars/globalStep.groovy
@@ -19,13 +19,13 @@ def findInNexus(Artifact artifact) {
             + "&sort=version"
             + "&direction=desc")
             .openConnection() as HttpURLConnection
-    connection.setRequestProperty('Accept', 'application/json')
-    if (connection.responseCode == 200) {
-        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
-        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
-        artifact.version = asset.maven2.version
-        artifact.url = asset.downloadUrl
-    } else {
-        throw new Exception("connection error")
-    }
+//    connection.setRequestProperty('Accept', 'application/json')
+//    if (connection.responseCode == 200) {
+//        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
+//        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
+//        artifact.version = asset.maven2.version
+//        artifact.url = asset.downloadUrl
+//    } else {
+//        throw new Exception("connection error")
+//    }
 }
