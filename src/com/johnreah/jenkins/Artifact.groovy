c2e7d6b216289ccad72acf8712f41f23469ef904
johnreah <john.reah@johnreah.com>
2021-02-15 18:14:19 +0000

    Experimenting with directories

--- src/com/johnreah/jenkins/Artifact.groovy
+++ src/com/johnreah/jenkins/Artifact.groovy
@@ -8,6 +8,6 @@ public class Artifact {
     String url
 
     String toString() {
-        return { -> "Artifact(${groupId?:'null'}, ${artifactId?:'null'}, ${packaging?:'null'}, ${version?:'null'}, ${url?:'null'})"}
+        return sprintf("ArtifactId(%s, %s, %s, %s, %s)", groupId, artifactId, packaging, version, url)
     }
 }
