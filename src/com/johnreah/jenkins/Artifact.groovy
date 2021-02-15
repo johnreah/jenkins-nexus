9be46084291977674cdfd3af671bd167da156dc7
johnreah <john.reah@johnreah.com>
2021-02-15 18:21:11 +0000

    Experimenting with directories

--- src/com/johnreah/jenkins/Artifact.groovy
+++ src/com/johnreah/jenkins/Artifact.groovy
@@ -8,11 +8,11 @@ public class Artifact {
     String url
 
     String toString() {
-        return sprintf("ArtifactId(%s)", groupId)
-        return sprintf("ArtifactId(%s)", artifactId)
-        return sprintf("ArtifactId(%s)", packaging)
-        return sprintf("ArtifactId(%s)", version)
-        return sprintf("ArtifactId(%s)", url)
+        println sprintf("ArtifactId(%s)", groupId)
+        println sprintf("ArtifactId(%s)", artifactId)
+        println sprintf("ArtifactId(%s)", packaging)
+        println sprintf("ArtifactId(%s)", version)
+        println sprintf("ArtifactId(%s)", url)
         return sprintf("ArtifactId(%s, %s, %s, %s, %s)", groupId, artifactId, packaging, version, url)
     }
 }
