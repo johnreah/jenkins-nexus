4873fa6bbf922f91f2c1f1f090f685d2baedf4c7
johnreah <john.reah@johnreah.com>
2021-02-15 17:55:45 +0000

    Experimenting with directories

--- src/com/johnreah/jenkins/Artifact.groovy
+++ src/com/johnreah/jenkins/Artifact.groovy
@@ -2,11 +2,15 @@ package com.johnreah.jenkins
 
 import groovy.transform.ToString
 
-@ToString
+//@ToString
 public class Artifact {
     String groupId
     String artifactId
     String packaging
     String version
     String url
+
+    String toString() {
+        return "TOSTRING artifactId: " + artifactId
+    }
 }
